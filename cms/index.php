c6e112547c6f779d7de97339a04250d383f5e4cc
Олег Никакой <oleg@iMac-Oleg.local>
2017-09-27 22:52:51 +0300

    creating cms

--- cms/index.php
+++ cms/index.php
@@ -11,6 +11,7 @@ if ($_POST['out']=="Выйти")
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="favicon.ico">
   <link href="css/fonts.css" rel="stylesheet" type="text/css" />
   <link href="css/style.css" rel="stylesheet" type="text/css" />
   <link href="css/media.css" rel="stylesheet" type="text/css" />
@@ -20,31 +21,118 @@ if ($_POST['out']=="Выйти")
 <body>
   <div class="wrapper">
     <div class="menu-wrapper">
-      <img src="images/label.jpg" alt="">
+      <a href="index.php">
+        <img src="images/label.jpg" alt="">
+      </a>
       <ul class="menu">
-        <li class="menu-item">
+        <li class="orders">
           <a href="">
             <img src="images/orders.png" alt="">заказы
           </a>
         </li>
-        <li class="menu-item">
+        <li class="users">
           <a href="">
             <img src="images/users.png" alt="">пользователи
           </a>
         </li>
-        <li class="menu-item">
+        <li class="items">
           <a href="">
             <img src="images/items.png" alt="">товары
           </a>
         </li>
-        <li class="menu-item">
+        <li class="categories">
           <a href="">
             <img src="images/categories.png" alt="">категории
           </a>
         </li>
       </ul>
     </div>
-    <div class="content">ЗАКАЗЫ</div>
+    <div class="content">
+      <h1>ЗАКАЗЫ</h1>
+      <table>
+        <tr>
+          <th>номер заказа</th>
+          <th>статус</th>
+          <th>сумма</th>
+          <th colspan="2">время заказа</th>
+        </tr>
+        <tr class="row">
+          <td class="item_1">
+            №4831 от slipknot666_666@mail.ru
+          </td>
+          <td class="item_2">
+            <select>
+              <option value="1">принят</option>
+              <option value="2">отгружен</option>
+              <option value="3">у курьера</option>
+              <option value="4">доставлен</option>
+              <option value="5">отмена</option>
+            </select>
+          </td>
+          <td class="item_3">
+            48 597 руб.
+          </td>
+          <td class="item_4">
+            25.09.2017 в 02:35
+          </td>
+          <td class="item_5">
+            <a href="">
+              просмотр
+            </a>
+          </td>
+        </tr>
+        <tr class="row">
+          <td class="item_1">
+            №4831 от slipknot666_666@mail.ru
+          </td>
+          <td class="item_2">
+            <select>
+              <option value="1">принят</option>
+              <option value="2">отгружен</option>
+              <option value="3">у курьера</option>
+              <option value="4">доставлен</option>
+              <option value="5">отмена</option>
+            </select>
+          </td>
+          <td class="item_3">
+            48 597 руб.
+          </td>
+          <td class="item_4">
+            25.09.2017 в 02:35
+          </td>
+          <td class="item_5">
+            <a href="">
+              просмотр
+            </a>
+          </td>
+        </tr>
+        <tr class="row">
+          <td class="item_1">
+            №4831 от slipknot666_666@mail.ru
+          </td>
+          <td class="item_2">
+            <select>
+              <option value="1">принят</option>
+              <option value="2">отгружен</option>
+              <option value="3">у курьера</option>
+              <option value="4">доставлен</option>
+              <option value="5">отмена</option>
+            </select>
+          </td>
+          <td class="item_3">
+            48 597 руб.
+          </td>
+          <td class="item_4">
+            25.09.2017 в 02:35
+          </td>
+          <td class="item_5">
+            <a href="">
+              просмотр
+            </a>
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </body>
 </html>

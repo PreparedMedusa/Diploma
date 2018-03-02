9a29416a1d63feee16f25c2531f51347143488a8
Oleg Sharapa <sharapa.oleg@gmail.com>
2018-03-03 02:04:28 +0300

    product page

---
+++ product.php
@@ -0,0 +1,26 @@
+<?php require 'db.php'; ?>
+  <!DOCTYPE html>
+  <html lang="ru">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="icon" type="image/gif" href="animated_favicon1.gif">
+    <link rel="stylesheet" href="css/swiper.min.css">
+    <link href="css/fonts.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/product_style.css" rel="stylesheet" type="text/css" />
+    <title>Super SHOP - Главная</title>
+  </head>
+
+  <body>
+    <div class="wrapper">
+      <header>
+        <?php require 'header.php'; ?>
+      </header>
+      <h1>Категория 1</h1>
+      <a href="#">вернуться в каталог</a>
+      <?php require 'footer.php'; ?>
+    </div>
+  </body>
